65bb322e59dc1572c16d8ecb3c577f71941d0cf3
user <end1_1@yahoo.com>
2023-04-12 22:07:12 +0400

    F

--- lib/screen/production/production_model.dart
+++ lib/screen/production/production_model.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:sartex/utils/http_sql.dart';
+import 'package:sartex/utils/text_editing_controller.dart';
 
 List<String> Lines = [
   '',
@@ -23,8 +25,82 @@ List<String> Lines = [
   'Alex'
 ];
 
+class ProductionItem {
+  final editBrand = STextEditingController();
+  final editModel = STextEditingController();
+  final editCommesa = STextEditingController();
+  final editCountry = STextEditingController();
+  final editColor = STextEditingController();
+  final List<String> brandLevel = [];
+  final List<String> modelLevel = [];
+  final List<String> commesaLevel = [];
+  final List<String> countryLevel = [];
+  final List<String> colorLevel = [];
+
+  ProductionItem() {
+    HttpSqlQuery.post({
+      "sl":
+          "select distinct(pd.brand) from Apranq a left join patver_data pd on pd.id=a.pid where pd.status='inProgress'"
+    }).then((value) {
+      brandLevel.clear();
+      for (var e in value) {
+        brandLevel.add(e['brand']);
+      }
+    });
+  }
+
+  Future<void> buildModelLevel(String brand) async {
+      HttpSqlQuery.post({
+        "sl":
+        "select distinct(pd.Model) from Apranq a left join patver_data pd on pd.id=a.pid where pd.status='inProgress' and brand='$brand'"
+      }).then((value) {
+        modelLevel.clear();
+        for (var e in value) {
+          modelLevel.add(e['Model']);
+        }
+      });
+  }
+
+  Future<void> buildCommesaLevel(String brand, String model) async {
+    var value = await HttpSqlQuery.post({
+      "sl":
+      "select distinct(pd.PatverN) from Apranq a left join patver_data pd on pd.id=a.pid where pd.status='inProgress' and brand='$brand' and Model='$model'"
+    });
+    commesaLevel.clear();
+    for (var e in value) {
+      commesaLevel.add(e['PatverN']);
+    }
+  }
+
+  Future<void> buildCountryLevel(String brand, String model, String commesa) async {
+    var value = await HttpSqlQuery.post({
+      "sl":
+      "select distinct(pd.country) from Apranq a left join patver_data pd on pd.id=a.pid where pd.status='inProgress' and brand='$brand' and Model='$model' and PatverN='$commesa'"
+    });
+    countryLevel.clear();
+    for (var e in value) {
+      countryLevel.add(e['country']);
+    }
+  }
+
+  Future<void> buildColorLevel(String brand, String model, String commesa, String country) async {
+    var value = await HttpSqlQuery.post({
+      "sl":
+      "select distinct(pd.Colore) from Apranq a left join patver_data pd on pd.id=a.pid where pd.status='inProgress' " +
+          "and brand='$brand' and Model='$model' and PatverN='$commesa' and country='$country'"
+    });
+    colorLevel.clear();
+    for (var e in value) {
+      colorLevel.add(e['Colore']);
+    }
+  }
+}
+
 class ProductionLine {
   String name = '';
+  final List<ProductionItem> items = [];
+
+  ProductionLine() {}
 }
 
 class ProductionModel {
@@ -33,6 +109,8 @@ class ProductionModel {
   final editDocN = TextEditingController();
   final StreamController linesController = StreamController();
   final List<ProductionLine> lines = [];
+
+  ProductionModel() {}
 
   Future<String> save() async {
     return '';
--- lib/screen/production/production_widget.dart
+++ lib/screen/production/production_widget.dart
@@ -8,36 +8,48 @@ import 'package:sartex/widgets/svg_button.dart';
 class ProductionWidget extends EditWidget {
   final _model = ProductionModel();
 
+  final InputDecoration labelDecor = const InputDecoration(
+      isDense: true,
+      border: InputBorder.none,
+      contentPadding: EdgeInsets.fromLTRB(3, 5, 3, 5));
+  final InputDecoration formDecor = const InputDecoration(
+      isDense: true,
+      border: InputBorder.none,
+      contentPadding: EdgeInsets.fromLTRB(5, 15, 5, 15));
+
   ProductionWidget({super.key, required String? DocN}) {
     _model.DocN = DocN;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Row(children: [
-        Expanded(
-            child: Text(L.tr('Production'),
-                textAlign: TextAlign.center, style: tsDialogHeader))
-      ]),
-      headerRow1(context),
-      lines(context),
-      Row(
-        children: [
-          Expanded(
-              child: Center(
-                  child: SvgButton(
-            onTap: () {
-              _model.lines.add(ProductionLine());
-              _model.linesController.add(null);
-            },
-            assetPath: 'svg/plus.svg',
-            darkMode: false,
-          )))
-        ],
-      ),
-
-    ]);
+    return SizedBox(
+        height: MediaQuery.of(context).size.height * 0.9,
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: Column(children: [
+          Row(children: [
+            Expanded(
+                child: Text(L.tr('Production'),
+                    textAlign: TextAlign.center, style: tsDialogHeader))
+          ]),
+          headerRow1(context),
+          Expanded(child: SingleChildScrollView(child: lines(context))),
+          Row(
+            children: [
+              Expanded(
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: SvgButton(
+                        onTap: () {
+                          _model.lines.add(ProductionLine());
+                          _model.linesController.add(null);
+                        },
+                        assetPath: 'svg/plus.svg',
+                        darkMode: false,
+                      )))
+            ],
+          ),
+        ]));
   }
 
   @override
@@ -92,13 +104,208 @@ class ProductionWidget extends EditWidget {
           return Column(
             children: [
               for (var e in _model.lines) ...[
-                Row(
-                  children: [
-                    Text(L.tr('Line')),
-                    Expanded(child: DropdownLine(productionLine: e))
-                  ],
-                )
-              ]
+                Container(
+                    margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    decoration: const BoxDecoration(color: Colors.blueAccent),
+                    child: Row(
+                      children: [
+                        Text(L.tr('Line')),
+                        const SizedBox(width: 10),
+                        Expanded(child: DropdownLine(productionLine: e)),
+                        SvgButton(
+                            onTap: () {
+                              e.items.add(ProductionItem());
+                              _model.linesController.add(null);
+                            },
+                            assetPath: 'svg/plus.svg'),
+                        const SizedBox(width: 10),
+                      ],
+                    )),
+                Column(children: [
+                  for (var l in e.items) ...[
+                    Container(
+                        margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        decoration: const BoxDecoration(color: Colors.black38),
+                        child: Row(children: [
+                          //BRAND
+                          Container(
+                            width: 120,
+                            decoration:
+                                const BoxDecoration(color: Colors.black38),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  readOnly: true,
+                                  decoration: labelDecor,
+                                  initialValue: L.tr('Brand'),
+                                ),
+                                TextFormField(
+                                  decoration: formDecor,
+                                  controller: l.editBrand,
+                                  onTap: () {
+                                    valueOfList(
+                                        context, l.brandLevel, l.editBrand,
+                                        done: () {
+                                      l
+                                          .buildModelLevel(l.editBrand.text)
+                                          .then((value) {
+                                        if (l.modelLevel.length == 1) {
+                                          l.editModel.text = l.modelLevel.first;
+                                        }
+                                      });
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                          //MODEL
+                          Container(
+                            width: 200,
+                            decoration:
+                                const BoxDecoration(color: Colors.black38),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  readOnly: true,
+                                  decoration: labelDecor,
+                                  initialValue: L.tr('Model'),
+                                ),
+                                TextFormField(
+                                  decoration: formDecor,
+                                  controller: l.editModel
+                                    ..addListener(() {
+                                      l
+                                          .buildCommesaLevel(l.editBrand.text,
+                                              l.editModel.text)
+                                          .then((value) {
+                                        if (l.commesaLevel.length == 1) {
+                                          l.editCommesa.text =
+                                              l.commesaLevel.first;
+                                        }
+                                      });
+                                    }),
+                                  onTap: () {
+                                    valueOfList(
+                                        context, l.modelLevel, l.editModel,
+                                        done: () {
+                                      l
+                                          .buildCommesaLevel(l.editBrand.text,
+                                              l.editModel.text)
+                                          .then((value) {
+                                        if (l.commesaLevel.length == 1) {
+                                          l.editCommesa.text =
+                                              l.commesaLevel.first;
+                                        }
+                                      });
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                          //COMMES
+                          Container(
+                            width: 100,
+                            decoration:
+                                const BoxDecoration(color: Colors.black38),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  readOnly: true,
+                                  decoration: labelDecor,
+                                  initialValue: L.tr('Order'),
+                                ),
+                                TextFormField(
+                                  decoration: formDecor,
+                                  controller: l.editCommesa
+                                    ..addListener(() {
+                                      l
+                                          .buildCountryLevel(
+                                              l.editBrand.text,
+                                              l.editModel.text,
+                                              l.editCommesa.text)
+                                          .then((value) {
+                                        if (l.countryLevel.length == 1) {
+                                          l.editCountry.text =
+                                              l.countryLevel.first;
+                                        }
+                                      });
+                                    }),
+                                  onTap: () {
+                                    valueOfList(
+                                        context, l.commesaLevel, l.editCommesa,
+                                        done: () {
+                                      l
+                                          .buildCountryLevel(
+                                              l.editBrand.text,
+                                              l.editModel.text,
+                                              l.editCommesa.text)
+                                          .then((value) {
+                                        if (l.countryLevel.length == 1) {
+                                          l.editCountry.text =
+                                              l.countryLevel.first;
+                                        }
+                                      });
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                          //COUNTRY
+                          Container(
+                            width: 100,
+                            decoration:
+                                const BoxDecoration(color: Colors.black38),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  readOnly: true,
+                                  decoration: labelDecor,
+                                  initialValue: L.tr('Country'),
+                                ),
+                                TextFormField(
+                                  decoration: formDecor,
+                                  controller: l.editCountry
+                                    ..addListener(() {
+                                      l
+                                          .buildColorLevel(
+                                              l.editBrand.text,
+                                              l.editModel.text,
+                                              l.editCommesa.text,
+                                              l.editCountry.text)
+                                          .then((value) {
+                                        if (l.colorLevel.length == 1) {
+                                          l.editColor.text = l.colorLevel.first;
+                                        }
+                                      });
+                                    }),
+                                  onTap: () {
+                                    valueOfList(
+                                        context, l.countryLevel, l.editCountry,
+                                        done: () {
+                                      l
+                                          .buildColorLevel(
+                                              l.editBrand.text,
+                                              l.editModel.text,
+                                              l.editCommesa.text,
+                                              l.editCountry.text)
+                                          .then((value) {
+                                        if (l.colorLevel.length == 1) {
+                                          l.editColor.text = l.colorLevel.first;
+                                        }
+                                      });
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                          )
+                        ]))
+                  ]
+                ])
+              ],
             ],
           );
         });
